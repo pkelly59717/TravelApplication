75eb195a969eb45837881037dd28145d7692a07d
Paul Kelly <paolok59717@gmail.com>
2018-03-06 20:33:50 +0000

    First Commit

---
+++ app/src/main/java/com/example/pkelly/travelapplication/DBConnect.java
@@ -0,0 +1,27 @@
+package com.example.pkelly.travelapplication;
+
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Created by paolo on 06-Mar-18.
+ */
+
+public class DBConnect {
+
+    public static Connection conn = null;
+
+    public static Connection ConnectDB () {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/mydayplanner", "root", "");
+            return conn;
+        }catch(Exception e) {
+            Log.v("unable to connect", e.getMessage());
+
+        }
+        return null;
+    }
+}
